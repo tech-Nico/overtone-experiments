f698097e2d7725e5310f68bf4c5d02518bbe3452
nicobalestra <nicobalestra@gmail.com>
2013-06-16 19:39:54 +0100

    First commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject overtone-experiments "0.1.0-SNAPSHOT"
+  :description "Just some random playing with Overtone"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.8.1"]])
---
+++ test/overtone_experiments/core_test.clj
@@ -0,0 +1,7 @@
+(ns overtone-experiments.core-test
+  (:use clojure.test
+        overtone-experiments.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
